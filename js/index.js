642fb2b8b86e58a30a7606a886dfcc59bb47ee73
dimka90 <dimkayilrit@gmail.com>
2022-11-16 05:30:53 +0100

    Code the function that makes a random for the computer to choose

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// function getComputerChoice(){
+
+//     let choice=["Rock","Papper","Scissor"];
+//         const userChoice= choice[Math.floor(Math.random()*3)];
+//         return userChoice;
+// }
+let getComputerChoice=()=>{
+    let choice=["Rock","Papper","Scissor"];
+        const userChoice= choice[Math.floor(Math.random()*3)];
+        return userChoice;
+        
+}
+
+
+
+// console.log(getComputerChoice())
+
+// console.log(Math.floor(Math.random()*5))
+
+
